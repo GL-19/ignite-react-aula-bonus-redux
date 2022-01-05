9afe5d1172fcf1a3ef3699213f7aa313943ddae0
GL-19 <loiolagl19@gmail.com>
2022-01-04 21:33:28 -0300

    Criada fake api com de produtos e estoque com json-server

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,32 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import api from "../services/api";
+import { IProduct } from "../store/modules/cart/types";
 
 const Catalog: FC = () => {
-	const catalog = useSelector((state) => state);
+	const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-	console.log(catalog);
+	useEffect(() => {
+		api.get("products").then((response) => {
+			setCatalog(response.data);
+		});
+	}, []);
 
-	return <h1>Catalog</h1>;
+	return (
+		<main>
+			<h1>Catalog</h1>
+
+			{catalog.map((product) => {
+				return (
+					<article key={product.id}>
+						<strong>{product.title}</strong> {"  -  "}
+						<span>{product.price}</span> {"   "}
+						<button type="button">Comprar</button>
+					</article>
+				);
+			})}
+		</main>
+	);
 };
 
 export default Catalog;
